b51948b00bc1cb7ea86e8c90191185d7ed7f0eeb
Ivan Maraev <ivanmaraev@internet.ru>
2025-03-04 18:59:26 +0300

    fix: service name

--- .devcontainer/run.sh
+++ .devcontainer/run.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 SCRIPT_DIR=$(dirname "$(realpath "$BASH_SOURCE")")
-docker compose -f $SCRIPT_DIR/docker-compose.yml up --build -d && docker compose -f $SCRIPT_DIR/docker-compose.yml exec dev bash
+echo $SCRIPT_DIR
+docker compose -f $SCRIPT_DIR/docker-compose.yml up --build -d && docker compose -f $SCRIPT_DIR/docker-compose.yml exec ros2 bash
